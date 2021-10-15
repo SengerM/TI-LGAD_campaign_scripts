ce390d347964157a1e39b2339ec5007c54f9d531
Matias Senger <m.senger@hotmail.com>
2021-10-15 19:39:08 +0200

    Now calculating the real distance

--- compare_devices.py
+++ compare_devices.py
@@ -10,8 +10,13 @@ data_df = pandas.DataFrame()
 for device_number in devices_that_have_been_measured_df.index:
 	temp_df = utils.read_measured_data_from(devices_that_have_been_measured_df.loc[device_number, 'Linear scan before irradiation'])
 	temp_df['#'] = str(device_number)
+	distances_df = utils.calculate_1D_scan_distance_from_dataframe(temp_df)
+	temp_df.set_index('n_position', inplace=True)
+	temp_df = temp_df.merge(distances_df, left_index=True, right_index=True)
 	data_df = data_df.append(temp_df, ignore_index=True)
+data_df.reset_index()
 
+data_df['Distance (m)'] -= data_df['Distance (m)'].mean()
 mean_df = data_df.groupby(by=['#','n_pulse','n_channel','n_position']).mean()
 std_df = data_df.groupby(by=['#','n_pulse','n_channel','n_position']).std()
 for column in std_df:
@@ -20,8 +25,8 @@ for column in std_df:
 mean_df.reset_index(inplace=True)
 
 fig = utils.line(
-	data_frame = mean_df[mean_df['n_pulse']==1],
-	x = 'n_position',
+	data_frame = mean_df.loc[mean_df['n_pulse']==1],
+	x = 'Distance (m)',
 	y = 'Collected charge (V s)',
 	color = '#',
 	symbol = 'n_channel',
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import pandas
 from pathlib import Path
 import plotly.express as px
 import plotly.graph_objs as go
+import numpy as np
 
 path_to_base_TI_LGAD = Path('/home/alf/cernbox/projects/4D_sensors/TI-LGAD_FBK_RD50_1')
 
@@ -62,3 +63,17 @@ def line(error_y_mode=None, **kwargs):
 			reordered_data.append(fig.data[i])
 		fig.data = tuple(reordered_data)
 	return fig
+
+def calculate_1D_scan_distance(positions):
+	"""positions: List of positions, e.g. [(1, 4, 2), (2, 5, 2), (3, 7, 2), (4, 9, 2)].
+	returns: List of distances starting with 0 at the first point and assuming linear interpolation."""
+	return [0] + list(np.cumsum((np.diff(positions, axis=0)**2).sum(axis=1)**.5))
+
+def calculate_1D_scan_distance_from_dataframe(df):
+	x = df.groupby('n_position').mean()[f'x (m)']
+	y = df.groupby('n_position').mean()[f'y (m)']
+	z = df.groupby('n_position').mean()[f'z (m)']
+	distances_df = pandas.DataFrame({'n_position': [i for i in range(len(set(df['n_position'])))], 'Distance (m)': calculate_1D_scan_distance(list(zip(x,y,z)))})
+	distances_df.set_index('n_position')
+	return distances_df
+	
