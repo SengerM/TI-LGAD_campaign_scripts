6dd58e2bfe0ca727964b7ce33b1155e4354e71b5
Matias Senger <m.senger@hotmail.com>
2022-01-04 11:42:29 +0100

    Improved script with arguments

--- fit_erf_and_calculate_calibration_factor.py
+++ fit_erf_and_calculate_calibration_factor.py
@@ -147,22 +147,29 @@ def script_core(measurement_name: str, window_size=125e-6):
 	fig.write_html(str(bureaucrat.processed_data_dir_path/Path(f'after_calibration.html')), include_plotlyjs = 'cdn')
 	
 if __name__ == '__main__':
+	import argparse
 	import measurements_table as mt
-	measurements_table_df = mt.create_measurements_table()
-	fit_results_df = pandas.DataFrame()
-	for measurement in sorted(measurements_table_df.index)[::-1]:
-		if mt.retrieve_measurement_type(measurement) == 'scan 1D':
-			if not (utils.path_to_measurements_directory/Path(measurement)/Path('fit_erf_and_calculate_calibration_factor')).is_dir():
-				print(f'Processing {repr(measurement)}...')
-				try:
-					script_core(measurement)
-				except Exception as e:
-					print(f'Cannot fit_erf to measurement {repr(measurement)}, reason: {repr(e)}.')
-			try:
-				this_measurement_data = pandas.read_csv(utils.path_to_measurements_directory/Path(measurement)/Path('fit_erf_and_calculate_calibration_factor/fit_results.csv'))
-				this_measurement_data['Measurement name'] = measurement
-				fit_results_df = fit_results_df.append(this_measurement_data, ignore_index = True)
-			except:
-				pass
-	with pandas.option_context('display.max_rows', None):
-		print(fit_results_df)
+	
+	parser = argparse.ArgumentParser(description='Fits an ERF function to the metal-silicon interfaces of a linear scan and uses this information to calculate a calibration factor based on the real distance.')
+	parser.add_argument(
+		'--dir',
+		metavar = 'path', 
+		help = 'Path to the base directory of a measurement. If "all", the script is applied to all linear scans.',
+		required = True,
+		dest = 'directory',
+		type = str,
+	)
+	args = parser.parse_args()
+	if args.directory.lower() != 'all':
+		script_core(Path(args.directory).parts[-1])
+	else:
+		measurements_table_df = mt.create_measurements_table()
+		for measurement_name in sorted(measurements_table_df.index)[::-1]:
+			if mt.retrieve_measurement_type(measurement_name) == 'scan 1D':
+				if not (utils.path_to_measurements_directory/Path(measurement_name)/Path('fit_erf_and_calculate_calibration_factor')).is_dir():
+					print(f'Processing {measurement_name}...')
+					try:
+						script_core(measurement_name)
+					except Exception as e:
+						print(f'Cannot process {measurement_name}, reason {repr(e)}...')
+				
