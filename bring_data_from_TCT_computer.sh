d071bb4a04a69a0ba01141359c274144dcad0275
Matias Senger <m.senger@hotmail.com>
2022-01-05 12:28:13 +0100

    Update

--- bring_data_from_TCT_computer.sh
+++ bring_data_from_TCT_computer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync tct@tct-computer.physik.uzh.ch:/home/tct/measurements_data/* /home/alf/cernbox/projects/4D_sensors/TI-LGAD_FBK_RD50_1/measurements_data --archive --recursive --verbose -P --times
+rsync --archive --recursive --verbose -P --times --delete tct@tct-computer.physik.uzh.ch:/home/tct/measurements_data/* /home/alf/cernbox/projects/4D_sensors/TI-LGAD_FBK_RD50_1/measurements_data
